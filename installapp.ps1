94d96dc98caaef93a8c918c64e010bb40a2485c3
hashmat MH <hashmat00@aol.com>
2020-01-20 15:40:42 -0800

    Update installapp.ps1

--- installapp.ps1
+++ installapp.ps1
@@ -1,6 +1,6 @@
 New-Item -ItemType Directory -Path 'C:\files'
 $folder = "c:\files"
-$url= "https://esta1esvdcartifacts01.blob.core.windows.net/scripts/shir/VC_redist.x64.exe?sp=rcw&st=2020-01-20T21:12:39Z&se=2020-01-22T05:12:39Z&spr=https&sv=2019-02-02&sr=b&sig=FRuuyrReHRNE6uykmcw%2BR8fEhax08m%2BQZGA1vCTqmbA%3D"
+$url= "https://cs4945cfaa111b8x4f18x8bb.blob.core.windows.net/scripts/VC_redist.x64.exe"
 $req = [System.Net.HttpWebRequest]::Create($url)
 $req.Method = "HEAD"
 $response = $req.GetResponse()
